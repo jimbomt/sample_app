cbff52abf9e8d5b64f1e3a73238ce7c1278e0fd2
jimbomt <james.scicluna@gmail.com>
2011-05-04 16:07:50 +0200

    Finished chapter 5, including exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   # The stuff below is for static pages (no longer needed)
   #get "pages/home"
   #get "pages/contact"  
   #get "pages/about"
   #get "pages/help"
   
-  root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
